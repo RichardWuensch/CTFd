f23901698361a0fe27af850ba9f7476941fede6c
Wünsch, Richard <richard.wuensch@student.fhws.de>
2024-02-22 10:20:02 +0100

    naming

--- CTFd/plugins/challenges/scripts_flag/changeFlag.py
+++ CTFd/plugins/challenges/scripts_flag/changeFlag.py
@@ -2,13 +2,11 @@ import subprocess
 import sys
 
 
-def restore_snapshot(vm_name, path_to_vdi, token):
-    print(path_to_vdi)
-    print(token)
+def restore_snapshot(vm_name, path_to_vdi, flag):
     subprocess.run(['VBoxManage', 'controlvm', vm_name, 'poweroff'])  # shutdown
     subprocess.run(['VBoxManage', 'snapshot', vm_name, 'restorecurrent'])  # reset on last snapshot
-    if path_to_vdi != "" and path_to_vdi != None:
-        subprocess.run([path_to_vdi + 'newToken.sh', token])  # call the script for the vm to set a new token
+    if path_to_vdi != "" and path_to_vdi is not None:
+        subprocess.run([path_to_vdi + 'newToken.sh', flag])  # call the script for the vm to set a new token
     subprocess.run(['VBoxManage', 'startvm', vm_name, '--type=headless'])  # start the vm
 
 
